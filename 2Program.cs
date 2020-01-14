b6b473c8c668c1dd58703692fa0aa6bd32f15a96
Alina Bram <46066719+AlinaBram@users.noreply.github.com>
2020-01-14 22:01:53 +0300

    Create 2Program.cs

---
+++ 2Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле 
+//I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите рост: ");
+            float h = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите вес: ");
+            float m = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Индекс массы тела: " + m / (h * h));
+
+            Console.ReadKey();
+        }
+    }
+}
