703a852d7f893887f11ca15af68e78b36bed2f1c
Alina Bram <46066719+AlinaBram@users.noreply.github.com>
2020-01-14 21:57:43 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, 
+//рост, вес). В результате вся информация выводится в одну строчку.
+namespace Homework1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string name;
+            string surname;
+            int age;
+            int height;
+            int weight;
+
+            Console.Write("Введите имя ");
+            name = Console.ReadLine();
+            Console.Write("Введите фамилию ");
+            surname = Console.ReadLine();
+            Console.Write("Введите возраст ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите рост ");
+            height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите вес ");
+            weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" "+name+" "+surname+" "+age+ " "+height+" "+weight);
+
+            Console.Read();
+        }
+    }
+}
