228b28c4104e807d00811b3ac4ba2ee37c6629dc
Alina Bram <46066719+AlinaBram@users.noreply.github.com>
2020-01-15 11:48:50 +0300

    Create 4aProgram.cs

---
+++ 4aProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Написать программу обмена значениями двух переменных:
+//а) с использованием третьей переменной
+namespace Homework4a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 6;
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("a = " + a + ", b = " + b + ", c = " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
