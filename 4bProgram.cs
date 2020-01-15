b1e1764590d5feabc946306db9c27829354bd8ff
Alina Bram <46066719+AlinaBram@users.noreply.github.com>
2020-01-15 11:49:32 +0300

    Create 4bProgram.cs

---
+++ 4bProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Написать программу обмена значениями двух переменных:
+//б) *без использования третьей переменной.
+namespace Homework4b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 4;
+
+            a = - a - b;
+            b = - a - b;
+            a = - a - b;
+
+            Console.WriteLine("a = "+a+" b = "+b);
+
+            Console.ReadKey();
+        }
+    }
+}
